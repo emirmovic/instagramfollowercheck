7ef72c999c9b4ae574009fd716ad617b33cf26ff
Emir Ibrisimovic <41544701+emirmovic@users.noreply.github.com>
2019-10-26 17:51:56 -0400

    Add files via upload

---
+++ instagram follower check.py
@@ -0,0 +1,86 @@
+import tkinter
+import tkinter as tk
+from tkinter import *
+from selenium.webdriver.common.keys import Keys
+
+from selenium import webdriver
+import time
+
+def goToProfile(driver, username):
+
+##    profile = driver.find_element_by_xpath("/html/body/span/section/nav/div[2]/div/div/div[3]/div/div[3]/a/svg/g/path")
+##    profile.click()
+
+    search = driver.find_element_by_xpath('/html/body/span/section/nav/div[2]/div/div/div[2]/input')
+    search.clear()
+    
+    
+    
+    
+
+def instagramLogin(usernameInput, passwordInput):
+    driver = webdriver.Chrome('/Users/emiribrisimovic/Desktop/chromedriver')
+    driver.get('https://www.instagram.com/accounts/login/?hl=en')
+    
+    time.sleep(1)
+    
+    username = driver.find_element_by_name("username")
+    username.clear()
+    username.send_keys(usernameInput)
+    
+    password = driver.find_element_by_name("password")
+    password.clear()
+    password.send_keys(passwordInput)
+
+    password.send_keys(Keys.RETURN)
+
+    time.sleep(2)
+
+    try:
+        notnow = driver.find_element_by_xpath("/html/body/div[3]/div/div/div[3]/button[1]")
+        notnow.click()
+    except:
+        pass
+
+    time.sleep(2)
+    
+##    goToProfile(driver)
+
+    search = driver.find_element_by_xpath('/html/body/span/section/nav/div[2]/div/div/div[2]/input')
+    search.clear()
+    search.send_keys(usernameInput)
+    time.sleep(2)
+    search.send_keys(Keys.TAB)
+    search.send_keys(Keys.ENTER)
+    
+
+def setVariables():
+    usernameInput = entry1.get()
+    passwordInput = entry2.get()
+    instagramLogin(usernameInput, passwordInput)
+    gotToProfile()
+    
+    
+master = tkinter.Tk()
+master.title("Instagram Input")
+
+entry1 = Entry(master)
+entry2 = Entry(master, show="*")
+
+
+tkinter.Label(master, text = "Username:").grid(row = 0) # this is placed in 0 0
+entry1.grid(row = 0, column = 1)
+
+tkinter.Label(master, text = "Password:").grid(row = 1)
+entry2.grid(row = 1, column = 1)
+
+
+    
+
+
+# 'Entry' is used to display the input-field
+
+button = Button(master, text="Submit", fg="red", command=setVariables)
+button.grid(row=3,column=1)
+
+master.mainloop()
